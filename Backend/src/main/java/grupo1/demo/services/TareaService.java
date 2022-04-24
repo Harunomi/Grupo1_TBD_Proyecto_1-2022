d44522e77f0c07de5af174b597489ce82923c25f
BenjaminParra <benjamin.parra.l@usach.cl>
2022-04-23 22:10:42 -0400

    [Benjamin Parra] - Se agrega TareaService.java

---
+++ Backend/src/main/java/grupo1/demo/services/TareaService.java
@@ -0,0 +1,54 @@
+package grupo1.demo.services;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import grupo1.demo.models.Tarea;
+import grupo1.demo.repositories.TareaRepository;
+
+public class TareaService {
+    private final TareaRepository tareaRepository;
+
+    TareaRepository(TareaRepository tareaRepository) {
+        this.tareaRepository = tareaRepository;
+    }
+
+    
+    @GetMapping("/tareas/count")
+    public String countTareas() {
+        int total = tareaRepository.countTareas();
+        return String.format("existen %s tareas", total);
+    }
+
+    // Create
+    @PostMapping("/tareas/create")
+    @ResponseBody
+    public String createTarea(@RequestBody Tarea tarea) {
+        return tareaRepository.createTarea(tarea);
+    }
+    
+    // Read all
+    @GetMapping("/tareas")
+    public List<Tarea> getAllTareas() {
+        return tareaRepository.getAllTareas();
+    }
+
+    // uUPODATE
+    @PostMapping("/tareas/updateTarea")
+    @ResponseBody
+    public String updateTarea(@RequestBody Tarea tarea) {
+        return tareaRepository.updateTarea(tarea);
+    }
+    // Delete
+    @DeleteMapping("/tarea/delete/{id}")
+    @ResponseBody
+    public boolean deleteTarea(@PathVariable int id) {
+        return tareaRepository.deleteTarea(id);
+    }
+}
